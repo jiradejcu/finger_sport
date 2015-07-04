a160924443a2d9af05409f5f65ce6854467d5376
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-07-04 15:21:08 +0700

    adjust character animator controller

---
+++ Finger Sport/Assets/Script/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	private Animator anim;
+	private float jumpTimer = 0;
+	
+	void Start ()
+	{
+		anim = GetComponentInChildren <Animator> ();
+	}
+
+	void Update ()
+	{
+		float acceleration = Time.deltaTime;
+		if (Input.GetKey (KeyCode.RightArrow))
+			acceleration = -acceleration;
+
+//		if(anim.GetInteger("Speed") < 0)
+//			acceleration = anim.GetInteger("Speed");
+
+//		anim.SetInteger ("Speed", anim.GetInteger("Speed") - acceleration);
+	}
+}

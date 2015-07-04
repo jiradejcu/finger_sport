c966f8dcc48b43e01bcfc62825ba4dadfe709013
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2015-07-04 16:48:30 +0700

    add character controller 2d

--- Finger Sport/Assets/Script/Character.cs
+++ Finger Sport/Assets/Script/Character.cs
@@ -3,23 +3,33 @@ using System.Collections;
 
 public class Character : MonoBehaviour
 {
-	private Animator anim;
-	private float jumpTimer = 0;
+	Animator anim;
+	Rigidbody2D rb;
+	float jumpTimer = 0;
+	const float defaultDeceleration = 0.05f;
+	const float defaultAcceleration = 0.2f;
 	
 	void Start ()
 	{
 		anim = GetComponentInChildren <Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update ()
+	void FixedUpdate ()
 	{
-		float acceleration = Time.deltaTime;
+		float acceleration = 0;
 		if (Input.GetKey (KeyCode.RightArrow))
-			acceleration = -acceleration;
+			acceleration = defaultAcceleration;
 
-//		if(anim.GetInteger("Speed") < 0)
-//			acceleration = anim.GetInteger("Speed");
+		if (acceleration > 0)
+			rb.velocity += new Vector2 (acceleration, 0);
+		else if(rb.velocity.magnitude > defaultAcceleration) {
+			Vector2 decelerationVector = (-1) * rb.velocity;
+			decelerationVector.Scale(new Vector2(defaultDeceleration, 0));
+			rb.velocity += decelerationVector;
+		} else
+			rb.velocity = Vector2.zero;
 
-//		anim.SetInteger ("Speed", anim.GetInteger("Speed") - acceleration);
+		Debug.Log(rb.velocity);
 	}
 }
